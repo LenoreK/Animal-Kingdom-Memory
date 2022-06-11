b6fc60103eaaad697b7e19da092b41ebbe3a4f69
Lenore Kelly <lenorekelly2@gmail.com>
2022-06-11 17:49:38 -0400

    added comments and new page called level-format.html

---
+++ app.js
@@ -0,0 +1,86 @@
+card = document.querySelector('.card')
+
+levelArray = [cardArrayLevelOne, cardArrayLevelTwo, cardArrayLevelThree]
+console.log(levelArray)
+
+//function that adds numbers to eat array - sends user to that game
+//populates screen with relevant array
+
+cardArrayLevelOne = [
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    },
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    }
+]
+
+cardArrayLevelTwo = [
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    },
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    },
+    {
+        name: 'bird',
+        img: 'Assets/bird.png'
+    },
+    {
+        name: 'bird',
+        img: 'Assets/bird.png'
+    }
+]
+
+cardArrayLevelThree = [
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    },
+    {
+        name: 'bear',
+        img: 'Assets/bear.png'
+    },
+    {
+        name: 'buffalo',
+        img: 'Assets/buffalo.png'
+    },
+    {
+        name: 'bird',
+        img: 'Assets/bird.png'
+    },
+    {
+        name: 'bird',
+        img: 'Assets/bird.png'
+    }   
+]
+
+function numberCard() {
+    for (let i = 0; i < card.length; i++) {
+        
+    }
+}
